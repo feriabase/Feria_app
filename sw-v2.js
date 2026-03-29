f13aac1ec2dba874ceb1fd3e3819935d5fd236d0
feriabase <feriabase3f@gmail.com>
2026-03-29 18:14:49 -0300

    Add new service worker version sw-v2.js

---
+++ sw-v2.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'feria-v2'; // 🔥 CAMBIAR VERSION SIEMPRE
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request));
+});
